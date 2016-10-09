9642e6fcffdee2627bef57f30033475e3b2194d8
Michael Conrads <michael.conrads@quartett-mobile.de>
2016-10-09 12:17:59 +0200

    added my-misc

---
+++ lisp/my-misc.el
@@ -0,0 +1,17 @@
+(defun add-format-json-to-js2-mode () (define-key js2-mode-map (kbd "C-x TAB") 'json-format))
+(add-hook 'js2-mode-hook 'add-format-json-to-js2-mode)
+
+(defun show-in-finder ()
+  (interactive)
+  (reveal-in-finder))
+
+(defun tum-address ()
+  (interactive)
+  (message "Boltzmannstr 3, 85748 München"))
+
+(defun qm-address ()
+  (interactive)
+  (message "Walter-Gropius-Straße 17, 80807 München"))
+
+(provide 'my-misc)
+
