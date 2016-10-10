7aac13739cd338178960858b375d4f54ba56603d
Michael Conrads <michael.conrads@quartett-mobile.de>
2016-10-10 08:01:31 +0200

    added macro keybindings

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -2,4 +2,10 @@
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
 (windmove-default-keybindings) ;; Shift+direction
 
+(global-set-key (kbd "C-c (") 'start-kbd-macro)
+(global-set-key (kbd "C-c )") 'end-kbd-macro)
+
+
 (provide 'keybindings)
+
+
