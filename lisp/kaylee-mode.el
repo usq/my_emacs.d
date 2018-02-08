79a2fef91bc0ea7af4a8417b4b5afb1137749b03
Michael Conrads <michael.conrads@quartett-mobile.de>
2018-02-08 12:18:16 +0100

    update kaylee mode

--- lisp/kaylee-mode.el
+++ lisp/kaylee-mode.el
@@ -21,59 +21,91 @@
   (setq font-lock-defaults '((kaylee-font-lock-keywords))))
 
 
-(defun kaylee-build ()
-  (interactive)
-  (_execKayleeShell "kaylee build"))
+(require 'subr-x)
+(define-derived-mode inferior-kaylee-mode comint-mode "Inferior Kaylee"
+  "Major mode for Kaylee inferior process."
+  (setq-local indent-tabs-mode nil)
 
+  ;; How to dispaly the process status in the mode-line
+  (setq mode-line-process '(":%s"))
+  
+  ;; This disables editing and traversing the "=>" prompts
+  (setq-local comint-prompt-read-only t)
+  ;; Lets comint mode recognize the prompt
+  ;;(setq-local comint-prompt-regexp (rx bol "=>" space))
 
-(defun kaylee-mount ()
-  (interactive)
-  (_execKayleeShell "kaylee mount"))
+  (defun kaylee--sentinel (process event)
+    (when (equal "finished" (string-trim event))
+      (with-current-buffer "*kaylee-output*")
+      (insert "\npress `q` to quit")
+      (special-mode)
+      ))
+  
+  (defun kaylee--run-cmd (args)
+    (if (get-buffer "*kaylee-output*")
+	(kill-buffer "*kaylee-output*"))
 
+    (let ((output (get-buffer-create "*kaylee-output*")))
+      (switch-to-buffer-other-window output)
 
-(defun kaylee-info (platform fw)
-  (interactive
-   "Mplatform: 
+      (let* ((kaylee-proc-buffer (apply 'make-comint-in-buffer "kaylee" output "kaylee" nil args))
+	     (kaylee-proc (get-buffer-process kaylee-proc-buffer)))
+	(set-process-sentinel kaylee-proc 'kaylee--sentinel))
+      ))
+  (defun kaylee--run-sync-cmd (args)
+    (if (get-buffer "*kaylee-output*")
+	(kill-buffer "*kaylee-output*"))
+
+    (let ((output (get-buffer-create "*kaylee-output*")))
+      (switch-to-buffer-other-window output)
+      (insert (shell-command-to-string (string-join (cons "kaylee" args) " ")))
+      ))
+  
+  (defun kaylee-build ()
+    (interactive)
+    (kaylee--run-cmd '("build")))
+
+  (defun kaylee-mount ()
+    (interactive)
+    (kaylee--run-cmd '("mount")))
+
+  (defun kaylee-fix-override ()
+    (interactive)
+    (kaylee--run-cmd '("fix" "--override")))
+
+  (defun kaylee-maintain ()
+    (interactive)
+    (kaylee--run-cmd '("maintain")))
+
+  (defun kaylee-force-maintain ()
+    (interactive)
+    (kaylee--run-cmd '("maintain" "--force")))
+
+  (defun kaylee-init-app ()
+    (interactive)
+    (kaylee--run-cmd '("init" "app")))
+
+  (defun kaylee-init-framework ()
+    (interactive)
+    (kaylee--run-cmd '("init" "framework")))
+
+  (defun kaylee-fix ()
+    (interactive)
+    (kaylee--run-cmd '("fix")))
+
+  (defun kaylee-info (platform fw)
+    (interactive
+     "Mplatform: 
 Mframework: ")
-  (_execKayleeShell (concat "kaylee info " platform " " fw)))
+    (kaylee--run-sync-cmd (list "info" platform fw))
+    
+    (with-current-buffer "*kaylee-output*"
+      (ansi-color-apply-on-region (point-min) (point-max))
+      (view-mode 1)
+      (goto-char (point-min))
+      ))
 
-
-(defun kaylee-fix-override ()
-  (interactive)
-  (_execKayleeShell "kaylee fix --override"))
-
-(defun kaylee-maintain ()
-  (interactive)
-  (_execKayleeShell "kaylee maintain"))
-
-(defun kaylee-force-maintain ()
-  (interactive)
-  (_execKayleeShell "kaylee maintain --force"))
-
-(defun kaylee-init-app ()
-  (interactive)
-  (_execKayleeShell "kaylee init app"))
-
-(defun kaylee-init-framework ()
-  (interactive)
-  (_execKayleeShell "kaylee init framework"))
-
-(defun kaylee-fix ()
-  (interactive)
-  (_execKayleeShell "kaylee" "fix"))
-
-
-
-(defun _execKayleeShell (&rest args)
-  (if (get-buffer "*kaylee-output*")
-      (kill-buffer "*kaylee-output*"))
-
-  (let ((output (get-buffer-create "*kaylee-output*")))
-    (eval `(start-process "kaylee process" output ,@args))
-    (switch-to-buffer-other-window output)
-    (special-mode)
-    )
-   )
+  )
 
 
 (defun kaylee-token ()
@@ -93,6 +125,10 @@ Mframework: ")
 
 
 
+
+
+
+;; toggle between catalyzers
 (require 'cl-lib)
 (defun kaylee--read-lines (filePath)
   "Return a list of lines of a file at filePath."
@@ -127,12 +163,10 @@ Mframework: ")
 	 (selparts (s-split " " selection))
 	 (fwname (first selparts))
 	 (fwplatform (second selparts)))
-	 (find-file-other-window (concat "Serenity/Projects/" fwplatform "/" fwname "/Catalyzer"))))
-
+    (find-file-other-window (concat "Serenity/Projects/" fwplatform "/" fwname "/Catalyzer"))))
 
 (require 'mc)
-(defun kaylee-run-tests ()
-  (interactive)
+(defun kaylee--run-tests (path-to-kaylee)
   (let ((buffname "kaylee tests"))
     (let ((buf (get-buffer-create buffname)))
       (with-current-buffer
@@ -141,13 +175,17 @@ Mframework: ")
 	(setq buffer-read-only nil)
 	(erase-buffer)
 	(special-mode)
-	(mc-async-cmd-logging "~/dev/QM/frameworks/serenity/kaylee/bin/kaylee selftest"
-			      buf)
+	(mc-async-cmd-logging (string-join (list  path-to-kaylee "selftest") " ") buf)
 	(switch-to-buffer-other-window buf)
-	)))
-  )
+	))))
 
+(defun kaylee-run-local-tests ()
+  (interactive)
+  (kaylee--run-tests "~/dev/QM/frameworks/serenity/kaylee/bin/kaylee"))
 
+(defun kaylee-run-tests ()
+  (interactive)
+    (kaylee--run-tests "kaylee"))
 
 (provide 'kaylee-mode)
 
